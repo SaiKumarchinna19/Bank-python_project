0b6b0ef549036cc6c2d6e6f090b35cf97d778205
Saikumar Bodepelly <chinnasaikumar24@gmail.com>
2025-08-24 16:58:25 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -17,7 +17,7 @@ obj3=Bank("CHINNI",4,1717515613,"current",7000,"thorrur",9951880148,"chinnasaiku
 obj4=Bank("POOJITHA",5,1717515614,"saivngs",8000,"thorrur",995580148,"chinnasaikumar@gmail.com","male",5919)
 obj5=Bank("NAGARAJU",6,1717515615,"current",9000,"hyd",9957780148,"chinnasaikumar@gmail.com","male",6919)
 obj6=Bank("RAMESH",7,1717515616,"saivngs",10000,"thorrur",995145450148,"chinnasaikumar@gmail.com","male",8919)
-obj7=Bank("mallik",8,1717515617,"current",11000,"punjagutta",99518756148,"chinnasaikumar@gmail.com","male",9919)
+obj7=Bank("MALLIK",8,1717515617,"current",11000,"punjagutta",99518756148,"chinnasaikumar@gmail.com","male",9919)
 obj8=Bank("mahesh",9,1717515618,"saivngs",12000,"srnagr",99518453148,"chinnasaikumar@gmail.com","male",7919)
 obj9=Bank("karan",10,1717515619,"current",13000,"thorrur",9951530148,"chinnasaikumar@gmail.com","male",3919)
 l=[obj,obj1,obj2,obj3,obj4,obj5,obj6,obj7,obj8,obj9]
@@ -25,6 +25,7 @@ l=[obj,obj1,obj2,obj3,obj4,obj5,obj6,obj7,obj8,obj9]
 # for i in l:
 #     # print(i.Name)
 #     print(i.id)
+
 
 
 
