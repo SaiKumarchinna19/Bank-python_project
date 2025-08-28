8f4ebd6416f928548ace596fe28781ac3ed90cfb
Saikumar Bodepelly <chinnasaikumar24@gmail.com>
2025-08-28 22:38:40 +0530

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -19,12 +19,13 @@ obj5=Bank("NAGARAJU",6,1717515615,"current",9000,"hyd",9957780148,"chinnasaikuma
 obj6=Bank("RAMESH",7,1717515616,"saivngs",10000,"thorrur",995145450148,"chinnasaikumar@gmail.com","male",8919)
 obj7=Bank("MALLIK",8,1717515617,"current",11000,"punjagutta",99518756148,"chinnasaikumar@gmail.com","male",9919)
 obj8=Bank("MAHESH",9,1717515618,"saivngs",12000,"srnagr",99518453148,"chinnasaikumar@gmail.com","male",7919)
-obj9=Bank("karan",10,1717515619,"current",13000,"thorrur",9951530148,"chinnasaikumar@gmail.com","male",3919)
+obj9=Bank("KARAN",10,1717515619,"current",13000,"thorrur",9951530148,"chinnasaikumar@gmail.com","male",3919)
 l=[obj,obj1,obj2,obj3,obj4,obj5,obj6,obj7,obj8,obj9]
 # print(l)
 # for i in l:
 #     # print(i.Name)
 #     print(i.id)
+
 
 
 
